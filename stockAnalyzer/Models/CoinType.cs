cf11238274d21f4ef67858683bf4ddab5d3b2978
Mike Han <hanm@usc.edu>
2021-11-10 01:15:20 -0800

    Initial Stock Analyzer

---
+++ stockAnalyzer/Models/CoinType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockAnalyzer
+{
+
+    internal class PriceCoinType : PriceBaseType
+    {
+        public int marketCap;
+        public string circulatingSupply;
+    }
+
+    internal class CoinType : BaseInvestmentType
+    {
+        new List<PriceCoinType> listPriceData = new List<PriceCoinType>();
+    }
+}
